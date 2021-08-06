ffa94d44d8663fd86cb9f36a2bd34256c9e7b2e8
Temirlan04 <zhochunbaev70@gmail.com>
2021-08-06 20:07:13 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double[] work = new double[6];
+        System.out.println(Arrays.toString(work));
+        work[0] = 1;
+        System.out.println(Arrays.toString(work));
+        work[1] = 2;
+        System.out.println(Arrays.toString(work));
+        work[2] = 3;
+        System.out.println(Arrays.toString(work));
+        work[3] = 4;
+        System.out.println(Arrays.toString(work));
+        work[4] = 5;
+        System.out.println(Arrays.toString(work));
+        work[5] = 6;
+        System.out.println(Arrays.toString(work));
+
+        double[] add = new double[7];
+        System.out.println(Arrays.toString(add));
+        add[0] = -1;
+        System.out.println(Arrays.toString(add));
+        add[1] = -2;
+        System.out.println(Arrays.toString(add));
+        add[2] = -3;
+        System.out.println(Arrays.toString(add));
+        add[3] = -4;
+        System.out.println(Arrays.toString(add));
+        add[4] = -5;
+        System.out.println(Arrays.toString(add));
+        add[5] = -6;
+        System.out.println(Arrays.toString(add));
+        add[6] = -7;
+        System.out.println(Arrays.toString(add));
+
+    }
+}
